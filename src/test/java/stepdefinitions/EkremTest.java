29120fae027588099e54553369cbb080b275df26
kose <ekoseqa@gmail.com>
2022-03-04 18:52:43 -0600

    Deneme1

---
+++ src/test/java/stepdefinitions/EkremTest.java
@@ -0,0 +1,12 @@
+package stepdefinitions;
+
+import org.junit.Test;
+
+public class EkremTest {
+    @Test
+    public void test(){
+        int say1 = 1;
+        int say2 = 2;
+        System.out.println("Iki sayiyi topla: " + (say1+say2));
+    }
+}
