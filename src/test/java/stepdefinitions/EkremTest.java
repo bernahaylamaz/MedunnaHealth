76d81a1e3a9a5c87dc0b0d874303b789d7f6abcb
kose <ekoseqa@gmail.com>
2022-03-04 19:30:19 -0600

    deneme2

--- src/test/java/stepdefinitions/EkremTest.java
+++ src/test/java/stepdefinitions/EkremTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 public class EkremTest {
     @Test
     public void test(){
-        int say1 = 1;
-        int say2 = 2;
+        int say1 = 3;
+        int say2 = 5;
         System.out.println("Iki sayiyi topla: " + (say1+say2));
+
+        System.out.println("Iki sayiyi carp: " + (say1*say2));
     }
 }
